904eb7f1104a75b8b9b89e29aaf37fb52f3f9e71
Alona <maksimenko.alona@gmail.com>
2020-08-07 21:19:07 +0200

    main page completed but without responsive design

--- main/main.js
+++ main/main.js
@@ -1,1 +1,28 @@
 
+// $('.parallax-window').parallax({imageSrc: '../images/island.jpg'});
+// jQuery(window).trigger('resize').trigger('scroll');
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        responsiveClass:true,
+        stagePadding: 23,
+        margin:25,
+        loop:true,
+        responsive:{
+              150:{
+                  items:1,
+              },
+              250:{
+                items:1,
+              },
+              600:{
+                  items:2,
+              },
+              1000:{
+                  items:4,
+              },
+              1200:{
+                  items:5,
+                }
+          }
+    });
+  });
