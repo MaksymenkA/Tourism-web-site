b397741f2aa54b7e7e51d87ea69f6b89f5d15a3c
Alona <maksimenko.alona@gmail.com>
2020-08-21 19:58:44 +0200

    final cleaning of main page

--- main/main.js
+++ main/main.js
@@ -1,46 +1,42 @@
-
-// $('.parallax-window').parallax({imageSrc: '../images/island.jpg'});
-// jQuery(window).trigger('resize').trigger('scroll');
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-        responsiveClass:true,
-        stagePadding: 23,
-        margin:25,
-        loop:true,
-        responsive:{
-              150:{
-                  items:1,
-              },
-              250:{
-                items:1,
-              },
-              600:{
-                  items:2,
-              },
-              1000:{
-                  items:4,
-              },
-              1200:{
-                  items:5,
-                }
-          }
-    });
-  });
-
-  function openMenu(){
-      var x = document.getElementById("contacts");
-      if (x.style.display === "block") {       
-        x.style.display = "none";
-      } else {
-        x.style.display = "block";
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    responsiveClass: true,
+    stagePadding: 23,
+    margin: 25,
+    loop: true,
+    responsive: {
+      150: {
+        items: 1,
+      },
+      250: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 4,
+      },
+      1200: {
+        items: 5,
       }
-  }
-  function openMainMenu(){
-    var x = document.getElementById("mainNav");
-    if (x.style.display === "block") {     
-      x.style.display = "none";
-        } else {
-      x.style.display = "block";
     }
+  });
+});
+
+function openMenu() {
+  var x = document.getElementById("contacts");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
- 
+function openMainMenu() {
+  var x = document.getElementById("mainNav");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
