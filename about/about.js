23f31b0f1789f2be54cf2f1d430cb0f47e4a39b8
Alona <maksimenko.alona@gmail.com>
2020-09-06 10:39:30 +0200

    completed responsive design for about page, used counteup plugin for animation, added animation with bar chart precentage

--- about/about.js
+++ about/about.js
@@ -1,25 +1,39 @@
 $(document).ready(function () {
-    $(".owl-carousel").owlCarousel({
-      responsiveClass: true,
-      stagePadding: 23,
-      margin: 25,
-      loop: true,
-      responsive: {
-        150: {
-          items: 1,
-        },
-        250: {
-          items: 1,
-        },
-        600: {
-          items: 2,
-        },
-        1000: {
-          items: 3,
-        },
-        1200: {
-          items: 4,
-        }
+  $("#owl-gallery").owlCarousel({
+    responsiveClass: true,
+    stagePadding: 23,
+    margin: 25,
+    loop: true,
+    responsive: {
+      150: {
+        items: 1,
+      },
+      250: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+      1200: {
+        items: 4,
       }
-    });
+    }
   });
+});
+
+$(document).ready(function () {
+  $("#opinion-carousel").owlCarousel({
+    responsiveClass: true,
+    stagePadding: 23,
+    margin: 25,
+    loop: true,
+    autoplay: true,
+    animateIn: 'fadeIn',
+    items: 1
+  });
+});
+
+
